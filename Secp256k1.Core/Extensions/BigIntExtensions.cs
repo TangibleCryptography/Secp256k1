53dd5174cd36a213665c645197843d2d8b9754cc
GeraldDavis <gerald@tangiblecryptography.com>
2014-04-14 16:15:26 -0400

    Moved and renamed BigIntExtensions to be consistent

--- Secp256k1.Core/Extensions/BigIntExtensions.cs
+++ Secp256k1.Core/Extensions/BigIntExtensions.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace Secp256k1
 {
-    public static class Extensions
+    public static class BigIntExtensions
     {
         public static BigInteger ModInverse(this BigInteger n, BigInteger p)
         {
@@ -11,32 +12,27 @@ namespace Secp256k1
             BigInteger y = 0;
             BigInteger a = p;
             BigInteger b = n;
-            BigInteger q, t;
-            BigInteger res;
+
             while (b != 0)
             {
-                t = b;
-                q = BigInteger.Divide(a, t);
+                BigInteger t = b;
+                BigInteger q = BigInteger.Divide(a, t);
                 b = a - q * t;
                 a = t;
                 t = x;
                 x = y - q * t;
                 y = t;
             }
+
             if (y < 0)
-            {
-                res = y + p;
-            }
-            else
-            {
-                res = y;
-            }
-            return res;
+                return y + p;
+            //else
+            return y;
         }
 
         public static bool TestBit(this BigInteger i, int n)
         {
-            int bitLength = i.BitLength();
+            //[resharper:unused local variable] int bitLength = i.BitLength();
             return !(i >> n).IsEven;
         }
 
@@ -46,7 +42,8 @@ namespace Secp256k1
             do
             {
                 bitLength++;
-            } while ((i >>= 1) != 0);
+            }
+            while ((i >>= 1) != 0);
             return bitLength;
         }
 
@@ -54,22 +51,32 @@ namespace Secp256k1
         {
             byte[] bytes = i.ToByteArray();
             if (bytes[bytes.Length - 1] == 0x00)
-            {
                 Array.Resize(ref bytes, bytes.Length - 1);
-            }
             if (bigEndian)
-            {
                 Array.Reverse(bytes, 0, bytes.Length);
-            }
 
             return bytes;
         }
 
+        public static BigInteger Order(this BigInteger b, BigInteger p)
+        {
+            BigInteger m = 1;
+            BigInteger e = 0;
+
+            while (BigInteger.ModPow(b, m, p) != 1)
+            {
+                m *= 2;
+                e++;
+            }
+
+            return e;
+        }
+
         private static BigInteger FindS(BigInteger p)
         {
-            BigInteger s, e;
-            s = p - 1;
-            e = 0;
+            BigInteger s = p - 1;
+            BigInteger e = 0;
+
             while (s % 2 == 0)
             {
                 s /= 2;
@@ -81,28 +88,13 @@ namespace Secp256k1
 
         private static BigInteger FindE(BigInteger p)
         {
-            BigInteger s, e;
-            s = p - 1;
-            e = 0;
+            BigInteger s = p - 1;
+            BigInteger e = 0;
+
             while (s % 2 == 0)
             {
                 s /= 2;
                 e += 1;
-            }
-
-            return e;
-        }
-
-
-        public static BigInteger Order(this BigInteger b, BigInteger p)
-        {
-            BigInteger m = 1;
-            BigInteger e = 0;
-
-            while (BigInteger.ModPow(b, m, p) != 1)
-            {
-                m *= 2;
-                e++;
             }
 
             return e;
@@ -152,42 +144,31 @@ namespace Secp256k1
 
         public static BigInteger ToBigIntegerUnsigned(this byte[] bytes, bool bigEndian)
         {
+            byte[] clone;
             if (bigEndian)
             {
                 if (bytes[0] != 0x00)
                 {
-                    byte[] clone = new byte[bytes.Length + 1];
+                    clone = new byte[bytes.Length + 1];
                     Buffer.BlockCopy(bytes, 0, clone, 1, bytes.Length);
                     Array.Reverse(clone);
-
                     return new BigInteger(clone);
                 }
-                else
-                {
-                    byte[] clone = new byte[bytes.Length];
-                    Buffer.BlockCopy(bytes, 0, clone, 0, bytes.Length);
-                    Array.Reverse(clone);
-
-                    return new BigInteger(clone);
-                }
+                clone = new byte[bytes.Length];
+                Buffer.BlockCopy(bytes, 0, clone, 0, bytes.Length);
+                Array.Reverse(clone);
+                return new BigInteger(clone);
             }
-            else
-            {
-                if (bytes[bytes.Length - 1] == 0x00)
-                {
-                    return new BigInteger(bytes);
-                }
-                else
-                {
-                    byte[] clone = new byte[bytes.Length + 1];
-                    Buffer.BlockCopy(bytes, 0, clone, 0, bytes.Length);
 
-                    return new BigInteger(clone);
-                }
-            }
+            if (bytes[bytes.Length - 1] == 0x00)
+                return new BigInteger(bytes);
+
+            clone = new byte[bytes.Length + 1];
+            Buffer.BlockCopy(bytes, 0, clone, 0, bytes.Length);
+            return new BigInteger(clone);
         }
 
-        public static string GetBitcoinAddress(this ECPoint publicKey, bool compressed=true)
+        public static string GetBitcoinAddress(this ECPoint publicKey, bool compressed = true)
         {
             var pubKeyHash = Hash160.Hash(publicKey.EncodePoint(compressed));
 
@@ -198,61 +179,43 @@ namespace Secp256k1
 
         public static BigInteger ShanksSqrt(this BigInteger a, BigInteger p)
         {
-
             if (BigInteger.ModPow(a, (p - 1) / 2, p) == (p - 1))
-            {
                 return -1;
 
-            }//No Sqrt Exists
-
             if (p % 4 == 3)
-            {
                 return BigInteger.ModPow(a, (p + 1) / 4, p);
-            }
 
             //Initialize 
-            BigInteger s, e;
-            s = FindS(p);
-            e = FindE(p);
+            BigInteger s = FindS(p);
+            BigInteger e = FindE(p);
+            BigInteger n = 2;
 
-            BigInteger n, m, x, b, g, r;
-            n = 2;
             while (BigInteger.ModPow(n, (p - 1) / 2, p) == 1)
-            {
                 n++;
-            }//Finds Generator
 
-            x = BigInteger.ModPow(a, (s + 1) / 2, p);
-            b = BigInteger.ModPow(a, s, p);
-            g = BigInteger.ModPow(n, s, p);
-            r = e;
-            m = b.Order(p);
-            if (m == 0)
-            {
-                return x;
-            }
+            BigInteger x = BigInteger.ModPow(a, (s + 1) / 2, p);
+            BigInteger b = BigInteger.ModPow(a, s, p);
+            BigInteger g = BigInteger.ModPow(n, s, p);
+            BigInteger r = e;
+            BigInteger m = b.Order(p);
 
-            //For Debugging
-            //Console.WriteLine("{0}, {1}, {2}, {3}, {4}",m, x, b, g, r);
+#if(DEBUG)
+            Debug.WriteLine("{0}, {1}, {2}, {3}, {4}", m, x, b, g, r);
+#endif
             while (m > 0)
             {
-
-               
-
                 x = (x * BigInteger.ModPow(g, TwoExp(r - m - 1), p)) % p;
                 b = (b * BigInteger.ModPow(g, TwoExp(r - m), p)) % p;
                 g = BigInteger.ModPow(g, TwoExp(r - m), p);
                 r = m;
                 m = b.Order(p);
-                //For Debugging
-                //Cnsole.WriteLine("{0}, {1}, {2}, {3}, {4}", m, x, b, g, r);
 
-
+#if(DEBUG)
+                Debug.WriteLine("{0}, {1}, {2}, {3}, {4}", m, x, b, g, r);
+#endif
             }
 
             return x;
-
-
         }
     }
 }
